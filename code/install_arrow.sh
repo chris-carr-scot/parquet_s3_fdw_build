8b47802f8c273ecb6befe72715753f91bbc08c16
chris-carr-scot <140832450+chris-carr-scot@users.noreply.github.com>
2024-04-09 15:39:23 +0100

    Update install_arrow.sh

--- code/install_arrow.sh
+++ code/install_arrow.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 sudo apt-get update
 sudo apt-get install -y -V ca-certificates lsb-release wget
 sudo wget https://apache.jfrog.io/artifactory/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/apache-arrow-apt-source-latest-$(lsb_release --codename --short).deb
